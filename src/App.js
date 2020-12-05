79989f2b3b5d3ba75e6acc3184a6d4d244b9df44
ナモすけ <dynamorollerbot@gmail.com>
2020-12-06 05:32:40 +0900

    bg changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
+import './index.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="bg-red-500">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
