dfc018e93bb5c52c6dd8250c31bd63a7f3f1a9fc
ナモすけ <dynamorollerbot@gmail.com>
2020-12-07 19:31:47 +0900

    styled format

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import logo from './logo.svg';
 import './index.css';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="bg-red-500">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header className="bg-gray-800 p-2 text-gray-100 text-center sticky top-0">PHP関数名しりとり</header>
+      <div className="flex items-center sm:container mx-auto">
+        <div className="text-gray-100 w-6 ml-2">You</div>
+        <div className="rounded-xl m-3 shadow-md" style={{ backgroundColor: 'hsl(120, 78%, 59%)' }}>
+          <div className="text-gray-900 p-2">acos</div>
+          <div className="text-gray-600 text-sm border-t border-gray-400">
+            <a href="https://www.php.net/manual/ja/function.acos.php" target="_blank" rel="noopener noreferrer" className="block w-full h-full p-2">
+              大文字小文字を区別しない &quot;自然順&quot; アルゴリズムでエントリをソートする
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="bg-gray-200 p-1 pb-3 fixed inset-x-0 bottom-0 flex text-lg">
+        <input type="text" inputmode="email" placeholder=" a から始まる関数名" className="px-1 flex-grow m-1 rounded" />
+        <button className="bg-green-400 shadow-inner w-11 m-1 rounded text-gray-100" style={{ backgroundColor: 'hsl(178, 100%, 39%)' }}>Go!</button>
+      </div>
     </div>
   );
 }
-
-export default App;
